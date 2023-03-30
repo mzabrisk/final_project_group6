0623e87870401b4478d4d245b295a3e593872400
mzabrisk <mattzabriskie@matts-air.lan>
2023-03-29 21:52:55 -0600

    adding alternate geojson file

--- dashboard/static/js/challenge_logic.js
+++ dashboard/static/js/challenge_logic.js
@@ -244,7 +244,7 @@ legend.onAdd = function() {
 
 
   // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
-  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function(data) {
+  d3.json("/Users/mattzabriskie/Desktop/Bootcamp/repos/final_project_group6/dashboard/static/resources/countries.geojson").then(function(data) {
     console.log(data)
 
     L.geoJson(data, {
